79fad3d32a2c600a925903fdf6673f429de9b22d
Kirill Taratun <taratunse@gmail.com>
2026-03-05 12:14:26 +0700

    fix: обновить стили Heading, Loader и globals.css

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -4,7 +4,7 @@ interface IHeadingProps {
 
 const Heading = ({ title }: IHeadingProps) => {
     return (
-        <div className='text-gray-50 md:text-4xl text-2xl pb-6 text-center'>
+        <div className='text-gray-50 md:text-4xl text-2xl pb-3 mb-3 border-b-1 border-gray-800'>
             { title }
         </div>
     );
--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -3,7 +3,7 @@ import { Loader as LoaderIcon} from "lucide-react";
 const Loader = () => {
     return (
         <div>
-            <LoaderIcon className='animate-spin w-10 h-10 text-white' />
+            <LoaderIcon className='animate-spin w-10 h-10 text-white m-auto' />
         </div>
     );
 };
