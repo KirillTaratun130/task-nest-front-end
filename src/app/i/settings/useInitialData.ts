afa2ceb721a08150a9c13008e208cb799a6f4096
Kirill Taratun <taratunse@gmail.com>
2026-03-06 16:01:03 +0700

    feat: добавить хук useInitialData для инициализации формы настроек

---
+++ src/app/i/settings/useInitialData.ts
@@ -0,0 +1,21 @@
+
+import {UseFormReset} from "react-hook-form";
+import {TypeUserForm} from "@/types/auth.types";
+import {useProfile} from "@/hooks/useProfile";
+import {useEffect} from "react";
+
+export const useInitialData = (reset: UseFormReset<TypeUserForm>) => {
+    const { data, isSuccess } = useProfile()
+
+    useEffect(() => {
+        if (isSuccess && data) {
+            reset({
+                email: data.user.email,
+                name: data.user.name,
+                breakInterval: data.user.breakInterval,
+                intervalsCount: data.user.intervalsCount,
+                workInterval: data.user.workInterval
+            })
+        }
+    }, [isSuccess, data]);
+}
