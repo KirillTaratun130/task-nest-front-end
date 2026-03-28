925c969755eac606b4eb6cae30b46f15467854e1
Kirill Taratun <taratunse@gmail.com>
2026-03-28 15:47:20 +0700

    refactor: убрать неиспользуемые импорты в useTasks

--- src/app/i/tasks/hooks/useTasks.ts
+++ src/app/i/tasks/hooks/useTasks.ts
@@ -1,8 +1,5 @@
 import {useQuery} from "@tanstack/react-query";
-import {userService} from "@/services/user.service";
 import {taskService} from "@/services/task.service";
-import {useEffect, useState} from "react";
-import {ITaskResponse} from "@/types/task.types";
 
 export const useTasks = () => {
     const { data, isLoading } = useQuery({
