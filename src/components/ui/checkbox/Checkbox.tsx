9cc2600195e013031c7f821f4c479afe978815a3
Kirill Taratun <taratunse@gmail.com>
2026-03-28 17:32:22 +0700

    feat: стилизовать компонент Checkbox с поддержкой пропсов

--- src/components/ui/checkbox/Checkbox.tsx
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -1,14 +1,31 @@
 interface ICheckboxProps {
-    onChange:
-    checked:
+    onChange: (value: boolean) => void
+    checked: boolean
 }
 
-const Checkbox = () => {
+const Checkbox = ({ onChange, checked }: ICheckboxProps) => {
     return (
-        <div>
+        <input
+            type='checkbox'
+            checked={checked}
+            onChange={e => onChange(e.target.checked)}
+            className='
+                defaultCheckbox
+                relative
+                h-5
+                w-5
+                appearance-none
+                rounded-md
+                border
+                border-input-border
+                bg-input-bg
+                transition-all
+                cursor-pointer
+                checked:border-primary
+                checked:bg-primary
+                hover:border-primary/60'
+        />
+    )
+}
 
-        </div>
-    );
-};
-
-export default Checkbox;
+export default Checkbox
