ab927b4dcab402f18069b58907a1d7e058005231
Kirill Taratun <taratunse@gmail.com>
2026-04-08 17:35:45 +0700

    feat: добавить компонент TransparentField с forwardRef

---
+++ src/components/ui/fields/TransparentField.tsx
@@ -0,0 +1,17 @@
+import {forwardRef, InputHTMLAttributes} from "react";
+import cn from "clsx";
+
+type TypeTransparentField =  InputHTMLAttributes<HTMLInputElement>
+
+const TransparentField = forwardRef<HTMLInputElement, TypeTransparentField>(
+        ({ className, ...rest }, ref) => {
+        return (
+            <input className={cn('bg-transparent border-none focus:outline-0 focus:shadow-transparent w-full', className)}
+                ref={ref} {...rest} />
+        )
+    }
+)
+
+TransparentField.displayName = 'TransparentField'
+
+export default TransparentField;
