798f5f4fce8600b5066b107243bc7804531bb7b7
Kirill Taratun <taratunse@gmail.com>
2026-04-08 20:57:31 +0700

    feat: реализовать ListView с DragDropContext и колонками

--- src/app/i/tasks/list-view/ListView.tsx
+++ src/app/i/tasks/list-view/ListView.tsx
@@ -1,9 +1,31 @@
+'use client'
+
+import {useTasks} from "@/app/i/tasks/hooks/useTasks";
+import {useTaskDnd} from "@/app/i/tasks/hooks/useTaskDnd";
+import {DragDropContext} from "@hello-pangea/dnd";
+import {COLUMNS} from "@/app/i/tasks/columns.data";
+import ListRowParent from "@/app/i/tasks/list-view/ListRowParent";
 
 const ListView = () => {
-    return (
-        <div>
+    const { items, setItems } = useTasks()
+    const { onDragEnd } = useTaskDnd()
 
-        </div>
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div className=''>
+                <div className=''>
+                    <div>Название задачи</div>
+                    <div>Дата выполнения</div>
+                    <div>Приоритет</div>
+                    <div></div>
+                </div>
+                <div className=''>
+                    { COLUMNS.map(column => (
+                        <ListRowParent id={column.id} label={column.label} items={items} setItems={setItems} key={column.id} />
+                    )) }
+                </div>
+            </div>
+        </DragDropContext>
     );
 };
 
