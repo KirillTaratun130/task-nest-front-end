68721feae8983356ee08083c37fa1ae3e5dc69af
Kirill Taratun <taratunse@gmail.com>
2026-04-10 00:31:00 +0700

    refactor: упростить синтаксис фильтров задач

--- src/app/i/tasks/filter-tasks.ts
+++ src/app/i/tasks/filter-tasks.ts
@@ -14,32 +14,32 @@ export const filterTasks = (
     switch (value) {
 
         case 'today':
-            return tasks?.filter(item => {
+            return tasks?.filter(item =>
                 dayjs(item.createdAt).isSame(FILTERS.today, 'day') && !item.isCompleted
-            })
+            )
 
         case 'tomorrow':
-            return tasks?.filter(item => {
+            return tasks?.filter(item =>
                 dayjs(item.createdAt).isSame(FILTERS.tomorrow, 'day') && !item.isCompleted
-            })
+            )
 
         case 'on-this-week':
-            return tasks?.filter(item => {
+            return tasks?.filter(item =>
                 !dayjs(item.createdAt).isSame(FILTERS.today) &&
                 !dayjs(item.createdAt).isSame(FILTERS.tomorrow) &&
                 dayjs(item.createdAt).isSameOrBefore(FILTERS['on-this-week']) && !item.isCompleted
-            })
+            )
 
         case 'on-next-week':
-            return tasks?.filter(item => {
+            return tasks?.filter(item =>
                 dayjs(item.createdAt).isAfter(FILTERS['on-this-week']) &&
                 dayjs(item.createdAt).isSameOrBefore(FILTERS['on-next-week']) && !item.isCompleted
-            })
+            )
 
         case 'later':
-            return tasks?.filter(item => {
+            return tasks?.filter(item =>
                 (dayjs(item.createdAt).isAfter(FILTERS['on-next-week']) || !item.createdAt) && !item.isCompleted
-            })
+            )
 
         default:
             return []
