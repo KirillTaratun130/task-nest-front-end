ee02a109a2be716e9d73233643fa2dcef6bb3bfe
Kirill Taratun <taratunse@gmail.com>
2026-04-20 21:47:38 +0700

    feat: подключить TimeBlockingList и добавить drag-and-drop

--- src/app/i/time-blocking/TimeBlocking.tsx
+++ src/app/i/time-blocking/TimeBlocking.tsx
@@ -3,14 +3,15 @@
 import {FormProvider, useForm} from "react-hook-form";
 import {TypeTimeBlockFromState} from "@/types/time-block.types";
 import TimeBlockingForm from "@/app/i/time-blocking/form/TimeBlockingForm";
+import TimeBlockingList from "@/app/i/time-blocking/TimeBlockingList";
 
 const TimeBlocking = () => {
     const methods = useForm<TypeTimeBlockFromState>()
 
     return (
         <FormProvider {...methods}>
-            <div className='grid grid-cols-2 gap-12'>
-                {/*<TimeBlockingList />*/}
+            <div className='grid grid-cols-2 gap-12 mt-12'>
+                <TimeBlockingList />
                 <TimeBlockingForm />
             </div>
         </FormProvider>
