21bdc2904c885c0d68cd3ef43fa2ed1e31f1d0c8
Kirill Taratun <taratunse@gmail.com>
2026-04-20 19:32:54 +0700

    fix: добавить поддержку кастомного className в Button

--- src/components/ui/buttons/Button.tsx
+++ src/components/ui/buttons/Button.tsx
@@ -1,10 +1,11 @@
 import {ButtonHTMLAttributes, PropsWithChildren} from "react";
+import cn from "clsx";
 
 type TypeButton = ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button = ( { children, ...rest }: PropsWithChildren<TypeButton> ) => {
+const Button = ( { children, className, ...rest }: PropsWithChildren<TypeButton> ) => {
     return (
-        <button className='text-text-heading bg-primary-dark px-4 py-2 rounded-md hover:bg-primary-hover cursor-pointer transition' {...rest}>{ children }</button>
+        <button className={cn('text-text-heading bg-primary-dark px-4 py-2 rounded-md hover:bg-primary-hover cursor-pointer transition', className)} {...rest}>{ children }</button>
     );
 };
 
